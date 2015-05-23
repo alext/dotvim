80128ccd2e10228e7a1ce4a5cea8a69e5c779e43
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-05-23 15:17:21 +0200

    Update install.sh to remove rvm specific stuff.
    Add a .ruby-version file to point ruby version managers at the system
    ruby.

--- install.rb
+++ install.rb
@@ -5,12 +5,10 @@
 home = File.expand_path('~')
 
 %w(vimrc).each do |file|
-  next if file =~ /install|readme/i
   target = File.join(home, ".#{File.basename(file)}")
   puts `ln -svf #{File.expand_path file} #{target}`
 end
 
 puts `git submodule update --init`
 
-puts `cd bundle/command-t; rvm use system; rake clobber; rake make`
-
+puts `cd bundle/command-t; rake clobber; rake make`
